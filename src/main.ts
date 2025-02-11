fbadf08e747a1ba94dc0db269e086d824d1b9f4e
Livi2992 <livinrinoshiya.lukdwinjesudhasan@chainsys.com>
2025-02-11 10:24:32 +0530

    last commit

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,14 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app.component';
+//  if app component is delared standlone true
+// import { bootstrapApplication } from '@angular/platform-browser';
+// import { appConfig } from './app/app.config';
+// import { AppComponent } from './app/app.component';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+// bootstrapApplication(AppComponent, appConfig)
+//   .catch((err) => console.error(err));
+
+// if app.modules is used.
+
+import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
+import { appModule } from "./app/app.module";
+
+platformBrowserDynamic().bootstrapModule(appModule);
